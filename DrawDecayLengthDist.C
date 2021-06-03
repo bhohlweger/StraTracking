2f6d39f45b659e92060b5e6abc3e852c8c87a4ad
bernie <bhohlweg@cern.ch>
2021-06-03 17:14:22 +0200

    Rework the decay length plot

--- DrawDecayLengthDist.C
+++ DrawDecayLengthDist.C
@@ -44,154 +44,78 @@ double decayLengthDist(double* x, double* par) {
   return TMath::Exp(-TMath::Sqrt(1+(mass/p)*(mass/p))/(c*tau)*L); 
 }
 
-/////////////////////////
-//helper functions ... //
-/////////////////////////
- 
-TCanvas* cc21(const char* c) {
-  //assumes canvas is used by 100% scaling                                                                                                                                                                                                    
-  TString cName = TString::Format("c%s", c);
-  TString p1Name = TString::Format("p1%s", c);
-  TString p2Name = TString::Format("p2%s", c);
-  auto c1 = new TCanvas(cName,cName,0,0,1143,600);
-  TPad* p1 = new TPad(p1Name,p1Name, 0, 0, 0.5, 1.0);
-  p1->SetLeftMargin(c1->GetLeftMargin()/0.5);
-  p1->SetRightMargin(c1->GetRightMargin()/0.5);
-  TPad* p2 = new TPad(p2Name,p2Name, 0.5, 0, 1.0, 1.0);
-  p2->SetLeftMargin(c1->GetLeftMargin()/0.5);
-  p2->SetRightMargin(c1->GetRightMargin()/0.5);
-  c1->cd();
-  p1->Draw();
-  p2->Draw();
-  return c1;
-}
-
-void StyleBox() {
-  const int NCont = 255;
-  //gROOT->ForceStyle();                                                                                                                                                                                                                      
-  gStyle->Reset("Plain");
-  gStyle->SetNumberContours(NCont);
-
-  gStyle->SetTitleBorderSize(0);
-  gStyle->SetOptStat(0);
-  gStyle->SetOptTitle(0);
-  gStyle->SetPalette(8, 0);
-  gStyle->SetCanvasBorderMode(0);
-
-  gStyle->SetPadTickX(1);
-  gStyle->SetPadTickY(1);
-
-  gStyle->SetPadBottomMargin(0.15);
-  gStyle->SetPadLeftMargin(0.1);
-  gStyle->SetPadTopMargin(0.15);
-  gStyle->SetPadRightMargin(0.05);
-
-  gStyle->SetFrameLineWidth(1);
-  gStyle->SetHistLineWidth(1);
-  gStyle->SetFuncWidth(2);
-  gStyle->SetLineWidth(2);
-  gStyle->SetHatchesLineWidth(1);
-  gStyle->SetHatchesSpacing(0.5);
-
-  //gStyle->SetErrorX(0.005);                                                                                                                                                                                                                 
-
-  gStyle->SetCanvasColor(0);
-  gStyle->SetPadColor(0);
-  gStyle->SetHistLineColor(kBlack);
-  gStyle->SetFuncColor(kGreen);
-  gStyle->SetLabelColor(kBlack, "xyz");
-  gStyle->SetPalette(kCividis);
-
-  gStyle->SetTextFont(43);
-  gStyle->SetLabelFont(43, "xyz");
-  gStyle->SetTitleFont(43, "xyz");
-  gStyle->SetLegendFont(43);
-
-  gStyle->SetTextSizePixels(28);
-  gStyle->SetLabelSize(28, "xyz");
-  gStyle->SetTitleSize(28, "xyz");
-  gStyle->SetLegendTextSize(28);
-
-  gStyle->SetLabelOffset(0.01, "xy");
-  gStyle->SetTitleOffset(1.2, "y");
-  gStyle->SetTitleOffset(1.25, "x");
-
-  gStyle->SetLegendBorderSize(0);
-  gStyle->SetLegendFillColor(kWhite);
-  gStyle->SetLegendBorderSize(0);
-
-
-}
-
-
-/////////////////////////
-//end helper functions //
-/////////////////////////
-
-
-void DrawDecayLengthDist() { 
-  StyleBox(); 
+void DrawDecayLengthDist() {  
   double tau_xi = 1.6695e-10;//s -> decay length of ~ 4.9 cm 
   double mass_xi = 1.322; //GeV/cc
   double tau_omega = 8.21e-11;//s -> decay length of ~ 2.7 cm 
   double mass_omega = 1.672; //GeV/cc
   double momentum = 1.0; //GeV/c
   
-  std::vector<float> layerPos = {0, 0.005, 0.012, 0.025, 0.0375, 0.070, 0.12, 0.20};
-  TF1* f_dcLngth = new TF1("DecLngth", decayLengthDist, 0, 0.5, 3); 
-  //draw the xi 
+  std::vector<float> layerPos = {0, 0.005, 0.012, 0.025, 0.0375, 0.070, 0.12};
+  TF1* f_dcLngth_Xi = new TF1("DecLngthXi", decayLengthDist, 0, 0.5, 3); 
+  TF1* f_dcLngth_Om = new TF1("DecLngthOmega", decayLengthDist, 0, 0.5, 3); 
 
   
-  auto c1 = cc21("1"); 
-  TPad* p1 = (TPad*)gROOT->FindObject("p11") ; 
-  TPad* p2 = (TPad*)gROOT->FindObject("p21") ; 
-  p1->cd(); 
-  auto d1 = p1->DrawFrame(0, 0, 0.1, 1.2); 
-  d1->SetTitle(";L (m);N(L)/N_{0} (Surv. Prob)");
+  auto c1 = c11("1"); 
+  TPad* p = (TPad*)gROOT->FindObject("p1") ; 
+  p->cd(); 
+  p->SetLogx(); 
+  auto d1 = p->DrawFrame(2e-3, 0, 0.2, 1.7); 
+  d1->SetTitle(";L (m);N(L)/N_{0} (Surv. Prob.)");
   d1->GetXaxis()->SetNdivisions(504); 
   d1->GetYaxis()->SetNdivisions(506); 
   d1->GetYaxis()->SetTitleOffset(1.4); 
-  f_dcLngth->SetParameters(tau_xi, mass_xi, momentum); 
-  f_dcLngth->SetLineColor(kPink+7); 
-  f_dcLngth->SetLineWidth(3);
-  f_dcLngth->DrawCopy("same");
+  f_dcLngth_Xi->SetTitle("#Xi^{#minus} (#it{c}#tau=4.9cm)");
+  f_dcLngth_Xi->SetParameters(tau_xi, mass_xi, momentum); 
+  f_dcLngth_Xi->SetLineColor(kPink+7); 
+  f_dcLngth_Xi->SetLineWidth(3);
+  f_dcLngth_Xi->DrawCopy("same");
   
-  TH1D* ProbHits_Xi = new TH1D("ProbHitsXi", "#Xi (#it{c}#tau = 4.9 cm)", layerPos.size()-1, 0, layerPos.size()); 
-  //Evaluate this at Layerpos to get the probability of a candidates surviving ... 
+  p->cd(); 
+  f_dcLngth_Om->SetTitle("#Omega^{#minus} (#it{c}#tau=2.5cm)");
+  f_dcLngth_Om->SetParameters(tau_omega, mass_omega, momentum); 
+  f_dcLngth_Om->SetLineColor(38); 
+  f_dcLngth_Om->SetLineWidth(3);
+  f_dcLngth_Om->DrawCopy("same");
+  
+  auto leg = p->BuildLegend( 0.62, 0.59, 0.92, 0.82, TString::Format("p = %.1f GeV/#it{c}", momentum).Data(), "l"); 
+  leg->SetFillStyle(0); 
+
+  TArrow layers; 
+  layers.SetLineColorAlpha(kBlack, 0.7); 
+  //layers.SetLineStyle(2);
+  layers.SetLineWidth(2);
+  
+  TLatex text; 
+  text.SetTextFont(43);
+  text.SetTextSizePixels(18); 
+  text.SetTextColor(1);
+
+  TArrow xiArr; 
+  xiArr.SetLineColorAlpha(kPink+7, 0.7);
+  xiArr.SetLineWidth(2); 
+  xiArr.SetLineStyle(2); 
+
+  TArrow omArr; 
+  omArr.SetLineColorAlpha(38, 0.7);
+  omArr.SetLineWidth(2); 
+  omArr.SetLineStyle(2); 
+
   for (int iLy = 1; iLy < layerPos.size(); ++iLy) { 
-    ProbHits_Xi->SetBinContent(iLy, f_dcLngth->Eval(layerPos[iLy])); 
-    ProbHits_Xi->GetXaxis()->SetBinLabel(iLy, TString::Format("Layer %d", iLy).Data()); 
+    if (iLy < 4) { 
+      layers.DrawArrow(layerPos[iLy], f_dcLngth_Xi->Eval(layerPos[iLy]), layerPos[iLy], f_dcLngth_Xi->Eval(layerPos[iLy])+0.12, 0.01, "<|"); 
+      layers.DrawArrow(layerPos[iLy], f_dcLngth_Om->Eval(layerPos[iLy]) -0.12, layerPos[iLy], f_dcLngth_Om->Eval(layerPos[iLy]), 0.01, "|>"); 
+      
+      text.DrawLatex(layerPos[iLy]*0.84, 
+		     (0.93*f_dcLngth_Xi->Eval(layerPos[iLy])+f_dcLngth_Om->Eval(layerPos[iLy]))/2., 
+		     TString::Format("Layer %d", iLy-1));
+      
+      xiArr.DrawArrow(layerPos[iLy], f_dcLngth_Xi->Eval(layerPos[iLy]), 0.2, f_dcLngth_Xi->Eval(layerPos[iLy]), 0.01, "|>"); 
+      omArr.DrawArrow(0, f_dcLngth_Om->Eval(layerPos[iLy]), layerPos[iLy], f_dcLngth_Om->Eval(layerPos[iLy]), 0.01, "<|"); 
+    }
+    
   }
-  p2->cd(); 
-  ProbHits_Xi->SetLineColor(kPink+7); 
-  ProbHits_Xi->SetMarkerColor(kPink+7); 
-  ProbHits_Xi->SetLineWidth(3);
-  ProbHits_Xi->GetXaxis()->SetNdivisions(504); 
-  ProbHits_Xi->GetXaxis()->SetTitle("");   
-  ProbHits_Xi->GetYaxis()->SetNdivisions(506); 
-  ProbHits_Xi->GetYaxis()->SetTitle("Surv. Prob at each Layer");   
-  ProbHits_Xi->GetYaxis()->SetTitleOffset(1.4); 
-  ProbHits_Xi->DrawCopy("");
-  
-  p1->cd(); 
-  f_dcLngth->SetParameters(tau_omega, mass_omega, momentum); 
-  f_dcLngth->SetLineColor(38); 
-  f_dcLngth->DrawCopy("same");
-  
-  TH1D* ProbHits_Omega = new TH1D("ProbHitsOmega", "#Omega (#it{c}#tau = 2.5 cm)", layerPos.size()-1, 0, layerPos.size()); 
-  //Evaluate this at Layerpos to get the probability of a candidates surviving ... 
-  for (int iLy = 1; iLy < layerPos.size(); ++iLy) { 
-    ProbHits_Omega->SetBinContent(iLy, f_dcLngth->Eval(layerPos[iLy])); 
-    ProbHits_Omega->GetXaxis()->SetBinLabel(iLy, TString::Format("Layer %d", iLy).Data()); 
-  }
-  ProbHits_Omega->SetLineColor(38); 
-  ProbHits_Omega->SetMarkerColor(38); 
-  ProbHits_Omega->SetLineWidth(3);
-  p2->cd(); 
-  ProbHits_Omega->DrawCopy("same");
-  
-  p2->BuildLegend( 0.453,0.636,0.753,0.846, TString::Format("p = %.1f GeV/#it{c}", momentum).Data()); 
-  
+
   c1->SaveAs("DecayLengthDist.pdf");
 
 }
