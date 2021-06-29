4f5a685d6a9726b3403c17d8259482dd84fabbf2
bernie <bhohlweg@cern.ch>
2021-06-29 11:14:35 +0200

    Makes the Comperator applicable to more situations

--- comparator.C
+++ comparator.C
@@ -1,12 +1,27 @@
 void comparator(TString addon) { 
   double xiccMass = 3.596; //3.621; 
-  double xiccWindow = 0.08;
+  double xiccWindow = 0.12;
   
-  TFile* xi = TFile::Open(    TString::Format("outxiccSelector_xi%s.root"  , addon.Data()), "read"); 
-  TFile* xic = TFile::Open(   TString::Format("outxiccSelector_xic%s.root" , addon.Data()), "read"); 
-  TFile* xicc = TFile::Open(  TString::Format("outxiccSelector_xicc%s.root", addon.Data()), "read"); 
+  std::vector<TFile*> inFiles; 
+
+  TSystemDirectory dir("ZeDirectory", gSystem->pwd());
+  auto files = dir.GetListOfFiles();
+  for (auto fileObj : *files)  {
+    auto file = (TSystemFile*) fileObj;
+    if (TString::Format("%s", file->GetName()).Contains(addon)) { 
+      std::cout << "Reading file " << file->GetName() << std::endl;
+      inFiles.emplace_back(TFile::Open(file->GetName(), "read"));
+    }
+  }
+
+
+  std::vector<int> colors = {kPink+7, 38, kGreen+3, kOrange+7, kBlack, kViolet+1, kCyan+1}; 
+  //std::vector<TString> histnames = {"#Xi^{#minus} + 3#times#pi_{Pythia}", "#Xi^{+}_{c} + #pi_{Pythia}", "#Xi_{cc}^{++}"}; 
+  std::vector<TString> histnames = {"0-2 GeV/#it{c}", "2-4 GeV/#it{c}", "4-6 GeV/#it{c}", "6-8 GeV/#it{c}", "8-10 GeV/#it{c}" }; 
+
   TFile* output = TFile::Open(TString::Format("outComp_cut%s.root"         , addon.Data()), "recreate"); 
-  TList* inList = xi->GetListOfKeys(); 
+  
+  TList* inList = inFiles[0]->GetListOfKeys(); 
   TIter next(inList); 
   TObject* obj = nullptr; 
   
@@ -15,62 +30,52 @@ void comparator(TString addon) {
     if (objName.Contains("Counter")  || objName.Contains("_vs_") ) { 
       continue; 
     }
-
-    TH1D* xiHist = (TH1D*)xi->Get(obj->GetName()); 
-    TH1D* xicHist = (TH1D*)xic->Get(obj->GetName()); 
-    TH1D* xiccHist = (TH1D*)xicc->Get(obj->GetName()); 
-    
-    xiHist->SetLineColor(kPink+7); 
-    xiHist->SetMarkerColor(kPink+7); 
-    xicHist->SetLineColor(38); 
-    xicHist->SetMarkerColor(38); 
-    xiccHist->SetLineColor(kGreen+3); 
-    xiccHist->SetMarkerColor(kGreen+3); 
-    
-    if (!(objName.Contains("mass") && objName.Contains("xi_cc"))) { 
-      xiHist->GetYaxis()->SetTitleOffset(1.2); 
-      xiHist->GetXaxis()->SetNdivisions(506); 
-      xiHist->GetYaxis()->SetNdivisions(504); 
-      /*
-      xiHist->Rebin(4); 
-      xicHist->Rebin(4); 
-      xiccHist->Rebin(4); 
-      */
-      xiHist->SetTitle("#Xi^{#minus} + 3#times#pi_{Pythia}"); 
-      xicHist->SetTitle("#Xi^{+}_{c} + #pi_{Pythia}"); 
-      xiccHist->SetTitle("#Xi_{cc}^{++}"); 
-
-      xiHist->Scale(1./xiHist->GetMaximum()); 
-      xicHist->Scale(1./xicHist->GetMaximum()); 
-      xiccHist->Scale(1./xiccHist->GetMaximum()); 
-    
-      xiHist->GetXaxis()->SetTitle(obj->GetName()); 
-      xiHist->GetYaxis()->SetRangeUser(0.,1.7); 
-      xiHist->GetYaxis()->SetTitle("Count Normalized to Maximum"); 
-    } else { 
+    if ((objName.Contains("mass") && objName.Contains("xi_cc"))) { 
       continue; 
     }
+
     auto c = c11(obj->GetName()); 
     auto p = (TPad*)gROOT->FindObject(TString::Format("p%s",obj->GetName()).Data()); 
     p->cd(); 
     
-    auto leg = new TLegend(0.16, 0.45, 0.56, 0.8, "#splitline{ALICE 3 (Layout v1) Full Simulation}{Pythia pp #sqrt{s} = 13 TeV + GEANT3}");
+    auto leg = new TLegend(0.16, 0.35, 0.56, 0.8, "#splitline{ALICE 3 (Layout v1) Full Simulation}{Pythia pp #sqrt{s} = 13 TeV + GEANT3}");
     leg->SetFillStyle(0); 
 
     p->cd(); 
-    leg->AddEntry(xiHist, xiHist->GetTitle(), "l");
-    leg->AddEntry(xicHist, xicHist->GetTitle(), "l");
-    leg->AddEntry(xiccHist, xiccHist->GetTitle(), "l");
-    xiHist->Draw("hist"); 
-    xicHist->Draw("histsame"); 
-    xiccHist->Draw("histsame"); 
+    
+    int fileCounter = 0; 
+    
+    for (auto it : inFiles) { 
+      TH1D* Hist = (TH1D*)it->Get(obj->GetName()); 
+      if (!Hist) { 
+	continue;
+      }
+   
+      Hist->SetLineColor(colors[fileCounter]); 
+      Hist->SetMarkerColor(colors[fileCounter]); 
+ 
+      Hist->GetYaxis()->SetTitleOffset(1.2); 
+      Hist->GetXaxis()->SetNdivisions(506); 
+      Hist->GetYaxis()->SetNdivisions(504); 
+	
+      Hist->SetTitle(histnames[fileCounter].Data()); 
+
+      Hist->Scale(1./Hist->GetMaximum()); 
+    
+      Hist->GetXaxis()->SetTitle(obj->GetName()); 
+      Hist->GetYaxis()->SetRangeUser(0.,2.5); 
+      Hist->GetYaxis()->SetTitle("Count Normalized to Maximum"); 
+      fileCounter==0?Hist->Draw("hist"):Hist->Draw("histsame"); 
+      leg->AddEntry(Hist, Hist->GetTitle(), "l");
+      fileCounter++;
+    }
     leg->Draw("same"); 
     output->cd();     
     c->Write();
     c->Close(); 
   }
   output->Close(); 
-  xi->Close(); 
-  xic->Close(); 
-  xicc->Close(); 
+  for (auto it : inFiles) { 
+    it->Close();
+  }
 }
